946f2be3626bd82d74f671bc90a9169f3adb3b88
superTO <to840116@gmail.com>
2022-03-20 14:15:15 +0800

    不透過selector直接在component subscribe

--- src/app/stores/todo-list.reducer.ts
+++ src/app/stores/todo-list.reducer.ts
@@ -1,17 +1,24 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import * as TodoListActions from './todo-list.actions';
+export interface todoItem {
+  id: string;
+  text: string;
+  done: string;
+}
 
 export const todoListFeatureKey = 'todoList';
 
 export interface State {
-
+  todoItems: todoItem[];
+  count: number;
 }
 
 export const initialState: State = {
-
+  todoItems: [],
+  count: 0
 };
 
-export const reducer = createReducer(
+export const todoListReducer = createReducer(
   initialState,
 
   on(TodoListActions.loadTodoLists, state => state),
